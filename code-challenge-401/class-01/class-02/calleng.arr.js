f81e5c38343c302f17c5e7b16b618f48cbe2f388
ahmad-ahmad <aa0796780751@gmail.com>
2021-05-17 23:45:21 +0300

    add code

--- code-challenge-401/class-01/class-02/calleng.arr.js
+++ code-challenge-401/class-01/class-02/calleng.arr.js
@@ -1,18 +1,25 @@
-let array = [1,2,3,5,6,7,89];
-function insertShiftArray (arr,number){
-  let newArr = [];
-  let middle = Number.parseInt(arr.length/2);
-  for (let i = 0; i<=arr.length-1; i++){
-    if(i === middle){
-      newArr.push(number);
-      newArr.push(arr[i]);
+
+
+
+let arrayData = [30,2,4,7,9,23,12];
+function insertShiftArray (array , num){
+  let newArrary =  [];
+
+
+  let center = Number.parseInt(array.length/2);
+
+
+  for (let i = 0; i<=array.length-1; i++){
+    if(i === center){
+      newArrary.push(num);
+      newArrary.push(array[i]);
     }
     else
-      newArr.push(arr[i]);
+      newArrary.push(array[i]);
   }
-  console.log('Original Array: ',arr);
-  console.log('Array with new number to the middle: ',newArr);
-  return newArr;
+
+  console.log(newArrary);
+  return newArrary;
 }
-insertShiftArray(array,4);
+insertShiftArray(arrayData,100);
 module.exports = insertShiftArray;
